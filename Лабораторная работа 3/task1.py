8ecf9f518b7b0d54b501851ff17ba4c09d412589
ms-spi <115370864+ms-spi@users.noreply.github.com>
2022-10-09 23:21:36 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task1.py
@@ -0,0 +1,8 @@
+list_ = [8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9]
+set_ = set(list_)
+unique = sum(set_)
+print(unique)
+num = len(set_)
+print (num)
+mean = sum(list_) / num
+print(round(mean, 5))
---
+++ Лабораторная работа 3/task2.py
@@ -0,0 +1,5 @@
+list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+mean = sum(list_numbers) / len(list_numbers)
+list_numbers[4] = mean
+# TODO заменить значение 5-го по порядку элемента средним арифметическим
+print(list_numbers)
