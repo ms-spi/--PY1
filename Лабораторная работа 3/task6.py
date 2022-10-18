f18ce579bbd7f89f3fc526aebe72add43a357545
ms-spi <115370864+ms-spi@users.noreply.github.com>
2022-10-18 22:17:37 +0300

    Update task6.py

--- Лабораторная работа 3/task6.py
+++ Лабораторная работа 3/task6.py
@@ -1,13 +1,13 @@
 list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 ind_max = 0
-max_ = list_numbers[ind_max]
+max_num = list_numbers[ind_max]
 
 
 for ind, val in enumerate(list_numbers):
-    if val >= max_:
-        max_ = val
+    if val >= max_num:
+        max_num = val
         ind_max = ind
 
-list_numbers[ind_max], list_numbers[-1] = list_numbers[-1], max_
+list_numbers[ind_max], list_numbers[-1] = list_numbers[-1], max_num
 
 print(list_numbers)
